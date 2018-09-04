ee842616dee924366fd5a53df865e5583ace8675
Maciej Krüger <mkg20001@gmail.com>
2018-09-04 21:45:52 +0200

    Finish adding to ipfs

--- mirror.sh
+++ mirror.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-xml=$(curl -s https://cdn.fwupd.org/downloads/firmware.xml.gz | gzip -d)
+set -e
+
+# Config
 
 OUT="$PWD/data"
+GATEWAY="https://ipfs.io/ipfs"
+
+# Code
+
+xml=$(curl -s https://cdn.fwupd.org/downloads/firmware.xml.gz | gzip -d) # get and ungz latest metadata
+
 mkdir -p "$OUT"
 
-for url in $(echo "$xml" | grep location | sed -r "s| *<location>(.+)</location>|\1|g"); do
-  f=$(basename "$url")
-  F="$OUT/$f"
-  if [ ! -e "$F" ]; then
-    wget "$url" -O "$F"
+for url in $(echo "$xml" | grep location | sed -r "s| *<location>(.+)</location>|\1|g"); do # iterate over locations
+  f=$(basename "$url") # get just the filename: "HASH-sth.cab"
+  F="$OUT/$f" # output location
+  if [ ! -e "$F.ipfs" ]; then # if not added to ipfs,
+    wget "$url" -O "$F" # download firmware
+    ipfs add -wQ "$F" > "$F.ipfs" # create hash for file wrapped with directory. wrapped so real filename can be used in url
   fi
+  hash=$(cat "$F.ipfs") # load hash
+  if [ -z "$hash" ]; then
+    echo "Hash missing for $f"
+    exit 2
+  fi
+  newURL=$(echo "$GATEWAY/$hash/$f") # generate new url
+  xml=$(echo -n "$xml" | sed "s|$URL|$newURL|g") # replace url in xml
 done
 
 echo -n "$xml" | gzip > "$OUT/firmware.xml.gz"
